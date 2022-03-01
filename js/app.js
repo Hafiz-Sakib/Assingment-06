53c0dce5e12b9fac90f17b101c1d856e316c2ba0
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-03-01 19:36:26 +0600

    card showed

--- js/app.js
+++ js/app.js
@@ -2,28 +2,32 @@ const searchPhone = () => {
   const searchInput = document.getElementById("search-input");
   const searchInputValue = searchInput.value;
   const error = document.getElementById("error");
-  //   if (isNaN(searchInputValue) || searchInputValue == "") {
-  //     error.innerText = "Give a number value 😢";
-  //     searchInput.value = "";
-  //   } else if (searchInputValue <= 0) {
-  //     error.innerText = "Give a Positive Value 😢";
-  //     searchInput.value = "";
-  //   } else if (searchInputValue > 52) {
-  //     error.innerText = "there is only 52 cards";
-  //   } else {
-  fetch(
-    `https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`
-  )
-    .then((res) => res.json())
-    .then((data) => displayPhones(data.data));
-  //   }
+  if (searchInputValue == "") {
+    error.innerText = "Please,Enter The Phone Name That You Searching For ☹️";
+    searchInput.value = "";
+    main.innerHTML = ``;
+  } else if (searchInputValue < 0 || searchInputValue > 0) {
+    error.innerText = "Enter The Model Name That You Searching For 🤔";
+    searchInput.value = "";
+    main.innerHTML = ``;
+  } else {
+    main.innerHTML = ``;
+    fetch(
+      `https://openapi.programming-hero.com/api/phones?search=${searchInputValue}`
+    )
+      .then((res) => res.json())
+      .then((data) => displayPhones(data.data));
+    searchInput.value = "";
+    error.innerHTML = ``;
+  }
 };
 
+//Display Phone Function
 const displayPhones = (phones) => {
   const first20Phones = phones.slice(0, 20);
+  const main = document.getElementById("main");
   for (const phone of first20Phones) {
     console.log(phone);
-    const main = document.getElementById("main");
     const newDiv = document.createElement("div");
     newDiv.classList.add("col");
     newDiv.innerHTML = `
@@ -32,7 +36,7 @@ const displayPhones = (phones) => {
       <div class="card-body">
         <h4 class="card-title text-danger fw-bolder"> ${phone.phone_name}</h4>
         <p class="card-text">Brand: ${phone.brand}</p>
-        <button class="btn btn-success" type="submit" id='details'>Details</button>
+        <button onclick = "phoneDetails()" class="btn btn-success" type="submit" id='details'>Details</button>
       </div>
       `;
     main.appendChild(newDiv);
